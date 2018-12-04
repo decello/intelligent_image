83d6c5f84a9d1f69b018a22e77df9ff804d0ae82
example <tugkanboz@Tugkans-MacBook-Pro.local>
2018-12-04 21:14:07 +0300

    initial commit

---
+++ intelligent image/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  intelligent image
+//
+//  Created by Tuğkan Boz on 4.12.2018.
+//  Copyright © 2018 Tuğkan Boz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
